82b97830db35983a6bb6d88ec65b36941a21ba0b
yanzhanjun <yanzhanjun@lattebank.com>
2021-01-28 14:07:54 +0800

    toy-react: step1

---
+++ packages/toy-react/webpack.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+  },
+  mode: 'development',
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+            ],
+            plugins: [
+              ['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}],
+            ],
+          },
+        },
+      },
+    ],
+  },
+}
